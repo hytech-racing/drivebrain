6f16a6998a1333381488d8168e5ecd6996cdb73b
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-08-01 20:12:41 +0000

    filter exterior triangles based on cone color

--- planning/include/PathPlanner.hpp
+++ planning/include/PathPlanner.hpp
@@ -41,8 +41,7 @@ namespace planning {
     coords.reserve(cones.cones_size()*2); //allocate enough memory to store the x and y coordinates of each cone
 
 
-    // TODO: pre process the data to alternate between yellow and blue cone coordinates
-
+    // pre-process data to alternate between yellow and blue cones
     int inner_index = 0;
     int outer_index = 2;
     for (int i = 0; i < cones.cones_size(); i++) {
@@ -58,17 +57,38 @@ namespace planning {
         outer_index +=4;
       }
 
-    }
+      // TODO: come up with something for orange cones (exit and entry lanes)
 
+    }
     
-    // adding each cone coordinate to a 2D vector needed to construct a delaunator object
-    for (auto& cone : cones.cones()) {
-      coords.push_back(cone.position().x()); 
-      coords.push_back(cone.position().y());
-      // not adding the z vector since that's assumed to be 0 
-    }
 
-    delaunator::Delaunator delaunay(coords); // triangulation happens on construction
+
+    delaunator::Delaunator delaunay(coords); // triangulation occurs on construction
+
+    //TODO: come up with a filter for cones being near each other in sequence or distance based
+
+    // filter out exterior triangles - check each triangle to see if it has at least one blue and yellow cone as vertices 
+    std::vector<std::size_t> filtered_triangles;
+
+    for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) { 
+      std::size_t a = delaunay.coords[2* delaunay.triangles[i]];
+      std::size_t b = delaunay.coords[2* delaunay.triangles[i+1]];
+      std::size_t c = delaunay.coords[2* delaunay.triangles[i+2]];
+
+      // check that at least one of the coordinates is based on a blue triangle
+      bool hasBlue = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_BLUE) || 
+                    (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_BLUE) ||
+                    (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_BLUE) ;
+
+      bool hasYellow = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_YELLOW) || 
+                    (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_YELLOW) ||
+                    (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_YELLOW) ;
+      
+      if (hasBlue && hasYellow) {
+        filtered_triangles.push_back(i); // starting index of the triangle
+      }
+      
+    }
 
     
 
