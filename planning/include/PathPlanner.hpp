2f4af294d2af6ff8a171e8ecfb87360612659fe6
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-08-03 03:36:07 +0000

    all logic implemented, idk if it works yet lmao

--- planning/include/PathPlanner.hpp
+++ planning/include/PathPlanner.hpp
@@ -2,6 +2,7 @@
 
 #include <random>
 #include <vector>
+#include <algorithm>
 
 #include <StateTracker.hpp>
 #include "dv_msgs.pb.h"
@@ -37,7 +38,8 @@ namespace planning {
   // function returns a vector of coordinates for the next target point in the path
   inline std::vector<core::xyz_vec<float>> plan_path(const dv_msgs::Cones& cones) {
     
-    std::vector<double> coords;
+    std::vector<core::xyz_vec<float>> path_points;
+    std::vector<double> coords; // must be a double for delaunator constructor
     coords.reserve(cones.cones_size()*2); //allocate enough memory to store the x and y coordinates of each cone
 
 
@@ -62,7 +64,6 @@ namespace planning {
     }
     
 
-
     delaunator::Delaunator delaunay(coords); // triangulation occurs on construction
 
     //TODO: come up with a filter for cones being near each other in sequence or distance based
@@ -71,7 +72,7 @@ namespace planning {
     std::vector<std::size_t> filtered_triangles;
 
     for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) { 
-      std::size_t a = delaunay.coords[2* delaunay.triangles[i]];
+      std::size_t a = delaunay.coords[2* delaunay.triangles[i]]; //don't need coords?
       std::size_t b = delaunay.coords[2* delaunay.triangles[i+1]];
       std::size_t c = delaunay.coords[2* delaunay.triangles[i+2]];
 
@@ -88,30 +89,53 @@ namespace planning {
         filtered_triangles.push_back(i); // starting index of the triangle
       }
       
+    }    
+
+    for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) {
+      
+      // check to see if the starting index of the current triangle is one of the kept triangles
+      std::size_t twin_edge = delaunay.halfedges[i]
+      if (!std::find(filtered_triangles.begin(), filtered_triangles.end(), delaunay.triangles[i]) != filtered_triangles.end() || 
+          !std::find(filtered_triangles.begin(), filtered_triangles.end(), twin_edge) != filtered_triangles.end()) {
+        continue;
+      }
+
+      if (delaunay.halfedges[i] != -1) {
+        double x0 = delaunay.coords[2* delaunay.triangles[i]];
+        double y0 = delaunay.coords[2* delaunay.triangles[i+1]];
+        double x1 = delaunay.coords[2* delaunay.triangles[twin_edge]];
+        double y1 = delaunay.coords[2* delaunay.triangles[twin_edge + 1]];
+
+        float mx = (x0 + x1) / 2.0;
+        float my = (y0 + y1) / 2.0;
+        float mz = 0.0;
+        core::xyz_vec<float> midpoint = {mx, my, mz};
+
+        path_points.push_back(midpoint);
+        
+        
+      }
+
     }
 
-    
+    return path_points;
 
-
-
-
-  //const ::hytech_msgs::xyz_vector cone_position = cones.cones().at(0).position();
 
 
     /* Below is a hardcoded, randomized set of points that renders a small path in Foxglove.
       The logic for Delauany triangulation should live in here (and operate on @cones). The end result should be a vector of xyz_vec.
       (In all cases, z should always be zero, since this is 2D Delauany. it's not like the car is gonna fly or anything.)
     */
-    static thread_local std::mt19937 rng{std::random_device{}()};
-    std::uniform_real_distribution<float> lateral(-3.0f, 3.0f);
+    // static thread_local std::mt19937 rng{std::random_device{}()};
+    // std::uniform_real_distribution<float> lateral(-3.0f, 3.0f);
 
 
-    return {
-      {0.0f, 0.0f, 0.0f},
-      {3.0f, lateral(rng), 0.0f},
-      {6.0f, lateral(rng), 0.0f},
-      {9.0f, lateral(rng), 0.0f}
-    };
+    // return {
+    //   {0.0f, 0.0f, 0.0f},
+    //   {3.0f, lateral(rng), 0.0f},
+    //   {6.0f, lateral(rng), 0.0f},
+    //   {9.0f, lateral(rng), 0.0f}
+    // };
   }
 
 }
