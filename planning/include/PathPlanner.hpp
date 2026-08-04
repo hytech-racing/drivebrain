0ea16f1dbcf5703c579a2b9ed7fb6be9915f8168
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-08-04 04:36:48 +0000

    new logic, no filtering

--- planning/include/PathPlanner.hpp
+++ planning/include/PathPlanner.hpp
@@ -11,39 +11,28 @@
 // left side = blue
 // right side = yellow
 
-/* 
-  1. locate cone coordinates - written to cone.position in dv_msg.pb -- done
-  2. form the delaunay triangulation -- done on construction of delaunator object
-  3. remove exterior triangles & make sure all triangles have a blue and yellow cone as two of its vertices - need the middle path
-  4. find the midpoints of all internal edges (blue to yellow across a path)
-  5. fit a spline through the midpoints 
-  6. smooth the trajectory -  compute curvature, then target speed
-
-
-  Extras: 
-  - either check that the z direction of cone location vectors are 0 or convert them to 0 regardless of the value
-  - need to figure out a way to handle the coordinate updates - make a delaunay object everytime seems like it would be computationally expensive
-*/
-
 
 namespace planning {
 
   /**
     Plans a drivable path through a known set of cones
-
+    
     @param cones The cones to plan through, map frame
     @return The planned path, map frame
   */  
 
-  // function returns a vector of coordinates for the next target point in the path
+  // Returns the next set of coordinates for the car to follow as a path
   inline std::vector<core::xyz_vec<float>> plan_path(const dv_msgs::Cones& cones) {
     
-    std::vector<core::xyz_vec<float>> path_points;
-    std::vector<double> coords; // must be a double for delaunator constructor
-    coords.reserve(cones.cones_size()*2); //allocate enough memory to store the x and y coordinates of each cone
+    std::vector<core::xyz_vec<float>> path_points; // THe final set of points to be followed
+    float mx = 0.0f;
+    float my = 0.0f;
 
+    std::vector<double> coords; // Cone coordinates used to make the delaunay triangulation
 
-    // pre-process data to alternate between yellow and blue cones
+    coords.resize(cones.cones_size()*2); // Allocate enough memory to store the x and y coordinates of each cone
+
+    // Pre-process the data to alternate between blue and yellow cones
     int inner_index = 0;
     int outer_index = 2;
     for (int i = 0; i < cones.cones_size(); i++) {
@@ -64,59 +53,88 @@ namespace planning {
     }
     
 
-    delaunator::Delaunator delaunay(coords); // triangulation occurs on construction
+    delaunator::Delaunator delaunay(coords); // Triangulation occurs on construction
 
-    //TODO: come up with a filter for cones being near each other in sequence or distance based
+    /*
+      Easier way to do it without filtering - just look for crossing edges (one end is blue, other end is yellow) 
+      and take the midpoint of that !!
+    */
 
-    // filter out exterior triangles - check each triangle to see if it has at least one blue and yellow cone as vertices 
-    std::vector<std::size_t> filtered_triangles;
+    std::size_t invalid_index = static_cast<std::size_t>(-1);
 
-    for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) { 
-      std::size_t a = delaunay.coords[2* delaunay.triangles[i]]; //don't need coords?
-      std::size_t b = delaunay.coords[2* delaunay.triangles[i+1]];
-      std::size_t c = delaunay.coords[2* delaunay.triangles[i+2]];
+    for (int i = 0; i < delaunay.triangles.size(); i++) {
 
-      // check that at least one of the coordinates is based on a blue triangle
-      bool hasBlue = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_BLUE) || 
-                    (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_BLUE) ||
-                    (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_BLUE) ;
-
-      bool hasYellow = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_YELLOW) || 
-                    (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_YELLOW) ||
-                    (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_YELLOW) ;
-      
-      if (hasBlue && hasYellow) {
-        filtered_triangles.push_back(i); // starting index of the triangle
-      }
-      
-    }    
-
-    for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) {
-      
-      // check to see if the starting index of the current triangle is one of the kept triangles
-      std::size_t twin_edge = delaunay.halfedges[i]
-      if (!std::find(filtered_triangles.begin(), filtered_triangles.end(), delaunay.triangles[i]) != filtered_triangles.end() || 
-          !std::find(filtered_triangles.begin(), filtered_triangles.end(), twin_edge) != filtered_triangles.end()) {
+      // If the edge is a boundary (-1) or a twin edge already iterated over, skip it
+      if (delaunay.halfedges[i] == invalid_index || delaunay.halfedges[i] > i) {
         continue;
       }
 
-      if (delaunay.halfedges[i] != -1) {
-        double x0 = delaunay.coords[2* delaunay.triangles[i]];
-        double y0 = delaunay.coords[2* delaunay.triangles[i+1]];
-        double x1 = delaunay.coords[2* delaunay.triangles[twin_edge]];
-        double y1 = delaunay.coords[2* delaunay.triangles[twin_edge + 1]];
+      std::size_t curr_edge = delaunay.triangles[i];
+      std::size_t twin_edge = delaunay.triangles[delaunay.halfedges[i]];
 
-        float mx = (x0 + x1) / 2.0;
-        float my = (y0 + y1) / 2.0;
-        float mz = 0.0;
-        core::xyz_vec<float> midpoint = {mx, my, mz};
+      auto curr_edge_color = cones.cones().at(curr_edge).color();
+      auto twin_edge_color = cones.cones().at(twin_edge).color();
+      
+      bool is_crossing_edge = (curr_edge_color == dv_msgs::Cones_ConeColor_BLUE && twin_edge_color == dv_msgs::Cones_ConeColor_YELLOW) ||
+          (curr_edge_color == dv_msgs::Cones_ConeColor_YELLOW && twin_edge_color == dv_msgs::Cones_ConeColor_BLUE);
 
-        path_points.push_back(midpoint);
-        
-        
+      if (is_crossing_edge) {
+        mx = (delaunay.coords[2* curr_edge] + delaunay.coords[2* twin_edge]) / (2.0);
+        my = (delaunay.coords[2* (curr_edge + 1)] + delaunay.coords[2* (twin_edge + 1)]) / (2.0);
       }
 
+      path_points.push_back({mx, my, 0.0f});
+
     }
+
+    // Filter out exterior triangles based on whether at least one of the vertices is a blue cone and at least one is a yellow cone 
+    // std::vector<std::size_t> filtered_triangles;
+
+    // for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) { 
+    //   std::size_t a = delaunay.coords[2* delaunay.triangles[i]]; 
+    //   std::size_t b = delaunay.coords[2* delaunay.triangles[i+1]];
+    //   std::size_t c = delaunay.coords[2* delaunay.triangles[i+2]];
+
+    //   bool hasBlue = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_BLUE) || 
+    //                 (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_BLUE) ||
+    //                 (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_BLUE) ;
+
+    //   bool hasYellow = (cones.cones().at(a).color() == dv_msgs::Cones_ConeColor_YELLOW) || 
+    //                 (cones.cones().at(b).color() == dv_msgs::Cones_ConeColor_YELLOW) ||
+    //                 (cones.cones().at(c).color() == dv_msgs::Cones_ConeColor_YELLOW) ;
+      
+    //   if (hasBlue && hasYellow) {
+    //     filtered_triangles.push_back(i); // Keep the starting index of the triangle that is kept
+    //   }
+    // }    
+
+      //TODO: come up with a filter for cones being near each other in sequence or distance based
+
+
+    // Looking for inner edges to take the midpoints of
+    // for (std::size_t i = 0; i < delaunay.triangles.size(); i+=3) {
+    //   std::size_t twin_edge = delaunay.halfedges[i];
+    //   if (std::find(filtered_triangles.begin(), filtered_triangles.end(), delaunay.triangles[i]) == filtered_triangles.end() || 
+    //       std::find(filtered_triangles.begin(), filtered_triangles.end(), twin_edge) == filtered_triangles.end()) {
+    //     continue;
+    //   }
+
+    //   if (delaunay.halfedges[i] != -1) {
+    //     double x0 = delaunay.coords[2* delaunay.triangles[i]];
+    //     double y0 = delaunay.coords[2* delaunay.triangles[i+1]];
+    //     double x1 = delaunay.coords[2* delaunay.triangles[twin_edge]];
+    //     double y1 = delaunay.coords[2* delaunay.triangles[twin_edge + 1]];
+
+    //     float mx = (x0 + x1) / 2.0;
+    //     float my = (y0 + y1) / 2.0;
+    //     float mz = 0.0;
+    //     core::xyz_vec<float> midpoint = {mx, my, mz};
+
+    //     path_points.push_back(midpoint);
+        
+    //   }
+
+    // }
 
     return path_points;
 
