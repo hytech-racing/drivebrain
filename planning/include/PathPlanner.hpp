34fab757d7603c8de2a67305c2dbbf09d4fce11d
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-08-30 01:44:15 +0000

    wip global position

--- planning/include/PathPlanner.hpp
+++ planning/include/PathPlanner.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <foxglove/FrameTransform.pb.h>
 #include <random>
 #include <vector>
 #include <algorithm>
@@ -27,9 +28,23 @@ namespace planning {
     std::vector<core::xyz_vec<float>> path_points; // THe final set of points to be followed
 
     if (cones.cones_size() < 3) return path_points; // Min 3 points required to do triangulation
-    
+  
+    // could use vndata lat and lon or estimator x/y
+
+    struct LocalCone {
+      float x, y;
+      dv_msgs::Cones_ConeColor color;
+    };
+    std::vector<LocalCone> filtered_cones;
+
     float mx = 0.0f;
     float my = 0.0f;
+
+    float max_range = 15.0f;
+    float half_fov_rad = static_cast<float>(M_PI)/1.5f;
+
+    float vehicle_x = static_cast<float>(foxglove::FrameTransform::default_instance().translation().x());
+    float vehicle_y = static_cast<float>(foxglove::FrameTransform::default_instance().translation().y());
 
     std::vector<double> coords; // Cone coordinates used to make the delaunay triangulation
 
@@ -37,13 +52,27 @@ namespace planning {
 
     // Store cones in message order, so point index i corresponds to cones().at(i)
     for (const auto& cone : cones.cones()) {
-      coords.push_back(cone.position().x());
-      coords.push_back(cone.position().y());
+
+      float x = cone.position().x();
+      float y = cone.position().y();
+      float r = std::hypot(x, y);
+      float angle = std::atan2(y, x);
+
+      if (r <= max_range && std::fabs(angle) <= half_fov_rad) {
+        filtered_cones.push_back(LocalCone{x, y, cone.color()});
+
+      // coords.push_back(cone.position().x());
+      // coords.push_back(cone.position().y());
+      }
+
+    if (filtered_cones.size() < 3) {
+      return path_points;
     }
-
-
-    // TODO: come up with something for orange cones (exit and entry lanes)
-
+    
+    for (const auto& c : filtered_cones) {
+      coords.push_back(c.x);
+      coords.push_back(c.y);
+    }
 
     delaunator::Delaunator delaunay(coords); // Triangulation occurs on construction
 
@@ -59,8 +88,11 @@ namespace planning {
       std::size_t curr_edge = delaunay.triangles[i];
       std::size_t twin_edge = delaunay.triangles[delaunay.halfedges[i]];
 
-      auto curr_edge_color = cones.cones().at(curr_edge).color();
-      auto twin_edge_color = cones.cones().at(twin_edge).color();
+      // auto curr_edge_color = cones.cones().at(curr_edge).color();
+      // auto twin_edge_color = cones.cones().at(twin_edge).color();
+
+      auto curr_edge_color = filtered_cones.at(curr_edge).color;
+      auto twin_edge_color = filtered_cones.at(twin_edge).color;
       
       bool is_crossing_edge = (curr_edge_color == dv_msgs::Cones_ConeColor_BLUE && twin_edge_color == dv_msgs::Cones_ConeColor_YELLOW) ||
           (curr_edge_color == dv_msgs::Cones_ConeColor_YELLOW && twin_edge_color == dv_msgs::Cones_ConeColor_BLUE);
@@ -92,6 +124,6 @@ namespace planning {
     //   {6.0f, lateral(rng), 0.0f},
     //   {9.0f, lateral(rng), 0.0f}
     // };
+    }
   }
-
 }
