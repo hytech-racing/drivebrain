627b2da989249d832191d97b29b4249261f90fa9
mansi-b29 <133283027+mansi-b29@users.noreply.github.com>
2026-09-01 02:39:32 +0000

    fov filtering & midpoint sorting wip

--- planning/include/PathPlanner.hpp
+++ planning/include/PathPlanner.hpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <vector>
 #include <algorithm>
+#include <numbers>
 
 #include <StateTracker.hpp>
 #include "dv_msgs.pb.h"
@@ -22,56 +23,50 @@ namespace planning {
     @return The planned path, map frame
   */  
 
+  inline constexpr double pi = 3.14159265358979323846;
+
   // Returns the next set of coordinates for the car to follow as a path
   inline std::vector<core::xyz_vec<float>> plan_path(const dv_msgs::Cones& cones) {
     
-    std::vector<core::xyz_vec<float>> path_points; // THe final set of points to be followed
+    /* Initializations */
+    std::vector<core::xyz_vec<float>> path_points; // The final set of points to be followed
+    path_points.clear();
+    
+    if (cones.cones_size() < 3) {
+      spdlog::error("not enough cones");
+      return path_points; // Min 3 points required to do triangulation
+    }
 
-    if (cones.cones_size() < 3) return path_points; // Min 3 points required to do triangulation
-  
-    // could use vndata lat and lon or estimator x/y
+    std::vector<core::xyz_vec<float>> midpoints; // Set of midpoints generated based on range filter
+    midpoints.clear();
 
-    struct LocalCone {
-      float x, y;
-      dv_msgs::Cones_ConeColor color;
-    };
-    std::vector<LocalCone> filtered_cones;
+    std::vector<double> coords; // Cone coordinates used to make the delaunay triangulation
+    coords.clear();
 
-    float mx = 0.0f;
-    float my = 0.0f;
+    float px = 0.0f;
+    float py = 0.0f;
 
-    float max_range = 15.0f;
-    float half_fov_rad = static_cast<float>(M_PI)/1.5f;
+    float max_range = 100.0f;
 
     float vehicle_x = static_cast<float>(foxglove::FrameTransform::default_instance().translation().x());
     float vehicle_y = static_cast<float>(foxglove::FrameTransform::default_instance().translation().y());
-
-    std::vector<double> coords; // Cone coordinates used to make the delaunay triangulation
+    
 
     coords.reserve(cones.cones_size()*2); // Allocate enough memory to store the x and y coordinates of each cone
 
     // Store cones in message order, so point index i corresponds to cones().at(i)
     for (const auto& cone : cones.cones()) {
 
-      float x = cone.position().x();
-      float y = cone.position().y();
-      float r = std::hypot(x, y);
-      float angle = std::atan2(y, x);
+      px = cone.position().x() - vehicle_x;
+      py = cone.position().y() - vehicle_y;
+      float magnitude = (px * px) + (py * py);
+      std::vector<float> point_to_vehicle = {px, py}; // vector between the 
+      float vector_angle = atan2(point_to_vehicle[1], point_to_vehicle[0]); // heading angle between the point and the car
 
-      if (r <= max_range && std::fabs(angle) <= half_fov_rad) {
-        filtered_cones.push_back(LocalCone{x, y, cone.color()});
-
-      // coords.push_back(cone.position().x());
-      // coords.push_back(cone.position().y());
+      if (vector_angle < pi/3.0 && magnitude < max_range) {
+        coords.push_back(cone.position().x());
+        coords.push_back(cone.position().y());
       }
-
-    if (filtered_cones.size() < 3) {
-      return path_points;
-    }
-    
-    for (const auto& c : filtered_cones) {
-      coords.push_back(c.x);
-      coords.push_back(c.y);
     }
 
     delaunator::Delaunator delaunay(coords); // Triangulation occurs on construction
@@ -88,25 +83,51 @@ namespace planning {
       std::size_t curr_edge = delaunay.triangles[i];
       std::size_t twin_edge = delaunay.triangles[delaunay.halfedges[i]];
 
-      // auto curr_edge_color = cones.cones().at(curr_edge).color();
-      // auto twin_edge_color = cones.cones().at(twin_edge).color();
-
-      auto curr_edge_color = filtered_cones.at(curr_edge).color;
-      auto twin_edge_color = filtered_cones.at(twin_edge).color;
+      auto curr_edge_color = cones.cones().at(curr_edge).color();
+      auto twin_edge_color = cones.cones().at(twin_edge).color();
       
       bool is_crossing_edge = (curr_edge_color == dv_msgs::Cones_ConeColor_BLUE && twin_edge_color == dv_msgs::Cones_ConeColor_YELLOW) ||
           (curr_edge_color == dv_msgs::Cones_ConeColor_YELLOW && twin_edge_color == dv_msgs::Cones_ConeColor_BLUE);
 
       if (is_crossing_edge) {
-        mx = (delaunay.coords[2* curr_edge] + delaunay.coords[2* twin_edge]) / (2.0);
-        my = (delaunay.coords[2*curr_edge + 1] + delaunay.coords[2* twin_edge + 1]) / (2.0);
-        path_points.push_back({mx, my, 0.0f});
+        float mx = (delaunay.coords[2* curr_edge] + delaunay.coords[2* twin_edge]) / (2.0);
+        float my = (delaunay.coords[2*curr_edge + 1] + delaunay.coords[2* twin_edge + 1]) / (2.0);
+        midpoints.push_back({mx, my, 0.0f});
       }
-
     }
 
+    float min_distance = 100.0f;
 
-    return path_points;
+    std::sort(midpoints.begin(), midpoints.end(), 
+      [&](const auto& point_a, const auto& point_b){
+        float ax = point_a.x - vehicle_x;
+        float ay = point_a.y - vehicle_y;
+        float bx = point_b.x - vehicle_x;
+        float by = point_b.y - vehicle_y;
+
+        float dist_a = (ax * ax) + (ay * ay);
+        float dist_b = (bx * bx) + (by * by);
+
+        return dist_a < dist_b;
+
+      });
+
+
+    for (const auto& midpoint : midpoints) {
+      path_points.push_back({midpoint.x, midpoint.y, 0.0f});
+    }
+
+      
+    // for (const auto& midpoint : midpoints) {
+    //   float mp_to_veh_x = midpoint.x - vehicle_x;
+    //   float mp_to_veh_y = midpoint.y - vehicle_y;
+
+    //   float mp_to_veh = (mp_to_veh_x * mp_to_veh_x) + (mp_to_veh_y * mp_to_veh_y);
+    //   if (mp_to_veh < min_distance) {
+    //     min_distance = mp_to_veh;
+    //     path_points.push_back({midpoint.x, midpoint.y, 0.0f});
+    //   }
+    // }
 
 
 
@@ -124,6 +145,8 @@ namespace planning {
     //   {6.0f, lateral(rng), 0.0f},
     //   {9.0f, lateral(rng), 0.0f}
     // };
-    }
+
+    return path_points;
+    
   }
 }
