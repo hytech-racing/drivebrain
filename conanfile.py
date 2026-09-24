77dae8c24ce07e6e9faea63e34d5faf5d14719a2
Mark Goltsman <mmgoltsman@mail.ru>
2026-09-24 19:46:53 -0400

    Pinned HT_Can, conflict fix

--- conanfile.py
+++ conanfile.py
@@ -20,7 +20,7 @@ class DrivebrainSoftware(ConanFile):
     def requirements(self): 
         self.requires("foxglove-websocket/1.4.0", transitive_headers=True)
         self.requires("protobuf/5.29.3", transitive_headers=True)
-        self.requires("boost/1.80.0", options={"header_only": True})
+        self.requires("boost/1.80.0")
         self.requires("spdlog/1.15.3")
         self.requires("mcap/2.0.2")
         self.requires("dbcppp/3.2.6")
